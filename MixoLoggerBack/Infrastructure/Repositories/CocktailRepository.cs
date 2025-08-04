a984a55b88d102a09d3428ba7c51b605d47b3886
Zerak630 <zerak.630@outlook.fr>
2025-08-04 15:46:59 +0200

    Stabilisation back

--- MixoLoggerBack/Infrastructure/Repositories/CocktailRepository.cs
+++ MixoLoggerBack/Infrastructure/Repositories/CocktailRepository.cs
@@ -6,8 +6,40 @@ namespace Infrastructure.Repositories;
 
 public class CocktailRepository : ICocktailRepository
 {
-	// Simule un stockage en mémoire
-	private static readonly ConcurrentDictionary<Guid, Cocktail> _store = new();
+	// Simule un stockage en mémoire avec une liste initiale de cocktails
+	private static readonly List<Cocktail> DEFAULT_COCKTAILS = [
+		new Cocktail("Mojito",
+        [
+            new(new Ingredient("Rum"), 50, UniteVolume.Mililitre),
+			new(new Ingredient("Mint"), 10, UniteVolume.Mililitre),
+			new(new Ingredient("Lime"), 20, UniteVolume.Mililitre),
+			new(new Ingredient("Sugar"), 15, UniteVolume.Mililitre),
+			new(new Ingredient("Soda Water"), 100, UniteVolume.Mililitre)
+		], "A refreshing cocktail with mint and lime."),
+		new Cocktail("Margarita",
+        [
+            new(new Ingredient("Tequila"), 50, UniteVolume.Mililitre),
+			new(new Ingredient("Lime Juice"), 30, UniteVolume.Mililitre),
+			new(new Ingredient("Triple Sec"), 20, UniteVolume.Mililitre)
+		], "A classic cocktail with tequila and lime."),
+		new Cocktail("Old Fashioned",
+        [
+            new(new Ingredient("Bourbon"), 50, UniteVolume.Mililitre),
+			new(new Ingredient("Sugar Cube"), 1, UniteVolume.Mililitre),
+			new(new Ingredient("Angostura Bitters"), 2, UniteVolume.Mililitre),
+			new(new Ingredient("Orange Peel"), 1, UniteVolume.Mililitre)
+		], "A timeless cocktail with whiskey and bitters."),
+		new Cocktail("Cosmopolitan",
+        [
+            new(new Ingredient("Vodka"), 40, UniteVolume.Mililitre),
+			new(new Ingredient("Triple Sec"), 20, UniteVolume.Mililitre),
+			new(new Ingredient("Cranberry Juice"), 30, UniteVolume.Mililitre),
+			new(new Ingredient("Lime Juice"), 10, UniteVolume.Mililitre)
+		], "A stylish cocktail with vodka and cranberry.")
+	];
+	private static readonly ConcurrentDictionary<Guid, Cocktail> _store = new(
+		DEFAULT_COCKTAILS.ToDictionary(c => c.Id)
+	);
 
 	public Task<Cocktail?> GetByIdAsync(Guid id)
 	{

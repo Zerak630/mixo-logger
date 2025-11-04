9b414d3777e615a7719d48644d8c566c650ce720
Zerak630 <zerak.630@outlook.fr>
2025-11-04 17:41:46 +0100

    Poc Menubar

--- MixoLoggerFront/src/app/app.ts
+++ MixoLoggerFront/src/app/app.ts
@@ -1,12 +1,32 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Menubar
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('MixoLoggerFront');
+
+  protected readonly menuItems: MenuItem[] = [
+    {
+      label: "Cocktails",
+      routerLink: "/cocktails"
+    },
+    {
+      label: "About",
+      routerLink: "/about"
+    },
+    {
+      label: "Contact",
+      routerLink: "/contact"
+    }
+  ];
 }

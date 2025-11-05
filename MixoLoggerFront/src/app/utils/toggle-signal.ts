a5e36f8973698dcd2a9467ec92da1a5b6c591e60
Zerak630 <zerak.630@outlook.fr>
2025-11-05 17:52:37 +0100

    Update togglSignal

--- MixoLoggerFront/src/app/utils/toggle-signal.ts
+++ MixoLoggerFront/src/app/utils/toggle-signal.ts
@@ -1,43 +1,48 @@
 import { signal, WritableSignal } from "@angular/core";
 
+interface ToggleableFunction {
+	/**
+	 * Creates a signal that can toggle between two values
+	 * @param initialValue Initial value
+	 * @param otherValue Value to toggle to
+	 */
+	<T extends string | number | symbol | object, U extends T>(initialValue: T, otherValue: U): ToggleableSignal<T | U>;
+	/**
+	 * Creates a boolean signal that can be toggled
+	 * @param initalState Initial boolean value
+	 */
+	(initalState: boolean): ToggleableSignal<boolean>;
+}
+
 /**
  * Extends WritableSignal to add toggle functionality
  */
 export interface ToggleableSignal<T> extends WritableSignal<T> {
-  /**
-   * Toggles between two values
-   */
-  toggle(): void;
+	/**
+	 * Interchange la valeur du signal
+	 */
+	toggle(): void;
+	/**
+	 * Réinitialise le signal à sa valeur initiale
+	 */
+	reset(): void;
 }
 
-/**
- * Creates a signal that can toggle between two values
- * @param initialValue Initial value
- * @param otherValue Value to toggle to
- */
-/**
- * Creates a boolean signal that can be toggled
- * @param initialValue Initial boolean value
- */
-export function toggle(initialValue: boolean): ToggleableSignal<boolean>;
-/**
- * Creates a signal that can toggle between two values
- * @param initialValue Initial value
- * @param otherValue Value to toggle to
- */
-export function toggle<T>(initialValue: T, otherValue: T): ToggleableSignal<T>;
-export function toggle<T>(initialValue: T, otherValue?: T): ToggleableSignal<T> {
-  const sig = signal(initialValue);
+function toggleSignal<T>(initialValue: T, otherValue?: T): ToggleableSignal<T> {
+	const sig = signal(initialValue);
 
-  if (typeof initialValue === 'boolean' && otherValue === undefined) {
-    // Type assertion nécessaire car TypeScript ne peut pas inférer que T est boolean
-    const boolSig = sig as WritableSignal<boolean>;
-    return Object.assign(boolSig, {
-      toggle: () => boolSig.set(!boolSig())
-    }) as ToggleableSignal<T>;
-  }
+	if (typeof initialValue === 'boolean' && otherValue === undefined) {
+		// Type assertion nécessaire car TypeScript ne peut pas inférer que T est boolean
+		const boolSig = sig as WritableSignal<boolean>;
+		return Object.assign(boolSig, {
+			toggle: () => boolSig.update(x => !x)
+		}) as ToggleableSignal<T>;
+	}
 
-  return Object.assign(sig, {
-    toggle: () => sig.set(sig() === initialValue ? otherValue as T : initialValue)
-  });
+	return Object.assign(sig, {
+		toggle: () => sig.update(x => x === initialValue ? otherValue as T : initialValue),
+		reset: () => sig.set(initialValue)
+	});
 }
+
+export const toggle: ToggleableFunction = (() => toggleSignal)();
